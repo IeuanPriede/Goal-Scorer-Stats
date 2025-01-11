4a87cda61d4a26d5b047c9185aa26c60c86cd90f
Ieuan Priede <priedeieuan@gmail.com>
2025-01-11 08:18:24 +0000

    Add a function that inputs a name into the sheet

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import gspread
 from google.oauth2.service_account import Credentials
 
+# Define the scope and authenticate
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -12,7 +13,19 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('Goal-Scorer-Stats')
 
+# Access the "stats" worksheet
 stats = SHEET.worksheet('stats')
+
+# Function to input data
+def add_name_to_sheet():
+    # Promt the user to enter a name
+    name = input("Enter players name: ")
+    # Appends the name to the sheet as a new row
+    stats.append_row([name])
+    print(f"Player '{name}' has been added to the sheet!")
+
+# Call the function
+add_name_to_sheet()    
 
 data = stats.get_all_values()
 
